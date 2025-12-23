f0437a9930555d227638af148516458d065ab3a3
Yeabseera Abebe <yeabebe41@gmail.com>
2025-12-23 21:39:24 +0300

    Implement routing code

---
+++ task3/src/routes/bookRoutes.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import {
+  getAllBooks,
+  getBookById,
+  createBook,
+  deleteBook,
+  searchBooks
+} from '../controllers/bookController.js';
+
+const router = express.Router();
+
+// Routing precedence matters
+router.get('/search', searchBooks);
+router.get('/', getAllBooks);
+router.get('/:id', getBookById);
+router.post('/', createBook);
+router.delete('/:id', deleteBook);
+
+export default router;
